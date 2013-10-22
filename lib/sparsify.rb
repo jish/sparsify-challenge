1eb279b9bc2f2dd24cd7cd34662224915407c427
Josh Lubaway <josh.lubaway@gmail.com>
2013-10-22 14:35:52 -0700

    Solution to step 1.

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -1,5 +1,38 @@
 # encoding: utf-8
 
 module Sparsify
-  # Your implementation goes here
+
+  def self.sparse(hash)
+    result = {}
+
+    hash.each do |key, value|
+      if value.is_a?(Hash)
+        prefixed = prefix_keys(key, value)
+        result.merge!(prefixed)
+      else
+        result[key] = value
+      end
+    end
+
+    result
+  end
+
+  def self.prefix_keys(prefix, hash)
+    result = {}
+    
+    hash.each do |key, value|
+      if value.is_a?(Hash)
+        prefixed = prefix_keys(key, value)
+        prefixed.each do |k, v|
+          result["#{prefix}.#{k}"] = v
+        end
+      else
+        result["#{prefix}.#{key}"] = value
+      end
+
+    end
+
+    result
+  end
+
 end
